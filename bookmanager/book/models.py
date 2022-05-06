9c35d8c31ae806e856849b6c98ddaee7cfb41aad
TangChaoQiang <2033445094@qq.com>
2022-05-06 21:59:51 +0800

    模型迁移

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    # 模型模块会自动创建id字段
+    name = models.CharField(max_length=12)
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
